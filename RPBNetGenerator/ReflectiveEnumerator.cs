bac9c2238557833d104e29631c95a574b7b25ab3
Alexander Hartmann-Kanter <devkanter@gmail.com>
2023-11-14 13:15:06 +0100

    sync

--- RPBNetGenerator/ReflectiveEnumerator.cs
+++ RPBNetGenerator/ReflectiveEnumerator.cs
@@ -1,28 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
 using Microsoft.CodeAnalysis;
 
 namespace RPBNetGenerator
 {
     public static class ReflectiveEnumerator
     {
-        private static List<INamedTypeSymbol> _symbols = new List<INamedTypeSymbol>();
+        private static readonly List<INamedTypeSymbol> _symbols = new List<INamedTypeSymbol>();
+
         public static IEnumerable<INamedTypeSymbol> GetEnumerableOfType(GeneratorExecutionContext context)
         {
-            var rpbCommon = context.Compilation.SourceModule.ReferencedAssemblySymbols.FirstOrDefault((x =>
-                x.Name == "SMG1Common"));
+            var rpbCommon = context.Compilation.SourceModule.ReferencedAssemblySymbols.FirstOrDefault(x =>
+                x.Name == "SMG1Common");
             _getAllSymbols(rpbCommon.GlobalNamespace);
-            return _symbols.Where(x => x.BaseType?.Name == "RPBPacket").ToList(); ;
+            return _symbols.Where(x => x.BaseType?.Name == "RPBPacket").ToList();
         }
 
         private static void _getAllSymbols(INamespaceSymbol namespaceSymbol)
         {
             _symbols.AddRange(namespaceSymbol.GetTypeMembers());
-            foreach (var namespaceMember in namespaceSymbol.GetNamespaceMembers())
-            {
-                _getAllSymbols(namespaceMember);
-            }
+            foreach (var namespaceMember in namespaceSymbol.GetNamespaceMembers()) _getAllSymbols(namespaceMember);
         }
     }
 }
